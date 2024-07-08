9da9729ec51555bf5424f2d0526866586d573d33
Andre Venditti <am7venditti@gmail.com>
2024-07-08 15:50:38 -0300

    First commit: screenmatch without web

---
+++ springProjects/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,27 @@
+package br.com.alura.screenmatch;
+
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=breaking+bad&apikey=ef6f5091");
+		System.out.println(json);
+
+		ConverteDados converteDados = new ConverteDados();
+		DadosSerie dadosSerie = converteDados.obertDados(json, DadosSerie.class);
+		System.out.println(dadosSerie);
+	}
+}
---
+++ springProjects/screenmatch/src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -0,0 +1,11 @@
+package br.com.alura.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosSerie(
+        @JsonAlias("Title") String titulo,
+        @JsonAlias("totalSeasons") int totalTemporadas,
+        @JsonAlias("imdbRating") String avaliacao) {
+}
