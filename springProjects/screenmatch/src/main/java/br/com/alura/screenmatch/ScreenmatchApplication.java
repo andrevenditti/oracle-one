80205ed126494b8bfa92e5f3bcbf90c11b02d3d8
Andre Venditti <am7venditti@gmail.com>
2024-07-08 20:46:31 -0300

    Feature: adding class 'Principal' and refactoring run inside 'application'

--- springProjects/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ springProjects/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,16 +1,9 @@
 package br.com.alura.screenmatch;
 
-import br.com.alura.screenmatch.model.DadosEpisodios;
-import br.com.alura.screenmatch.model.DadosSerie;
-import br.com.alura.screenmatch.model.DadosTemporada;
-import br.com.alura.screenmatch.service.ConsumoApi;
-import br.com.alura.screenmatch.service.ConverteDados;
+import br.com.alura.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -21,28 +14,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=breaking+bad&apikey=ef6f5091");
-		System.out.println(json);
-
-		//dados da serie
-		ConverteDados converteDados = new ConverteDados();
-		DadosSerie dadosSerie = converteDados.obterDados(json, DadosSerie.class);
-		System.out.println(dadosSerie);
-
-		//dados de um episodio consumido da API
-		json = consumoApi.obterDados("https://www.omdbapi.com/?t=breaking+bad&Season=2&episode=2&apikey=ef6f5091");
-		DadosEpisodios dadosEpisodios = converteDados.obterDados(json, DadosEpisodios.class);
-		System.out.println(dadosEpisodios);
-
-		//dados de uma temporada da api
-		List<DadosTemporada> temporadas = new ArrayList<>();
-		for (int i = 1; i <= dadosSerie.totalTemporadas() ; i++) {
-
-			json = consumoApi.obterDados("https://www.omdbapi.com/?t=breaking+bad&season=" + i + "&apikey=ef6f5091");
-			DadosTemporada dadosTemporada = converteDados.obterDados(json, DadosTemporada.class);
-			temporadas.add(dadosTemporada);
-		}
-		temporadas.forEach(System.out::println);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
--- springProjects/screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ springProjects/screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,6 +1,37 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.model.DadosEpisodios;
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverteDados;
+import br.com.alura.screenmatch.model.DadosTemporada;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Principal {
+    private Scanner leitura = new Scanner(System.in);
+    private final String ENDERECO = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=ef6f5091";
+    private ConsumoApi consumoApi = new ConsumoApi();
+    private ConverteDados converteDados = new ConverteDados();
     public void exibeMenu() {
+        System.out.println("Digite o nome da serie para busca: ");
+        var nomeSerie = leitura.nextLine();
+        var json = consumoApi.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+        System.out.println(json);
+
+        DadosSerie dadosSerie = converteDados.obterDados(json, DadosSerie.class);
+        System.out.println(dadosSerie);
+
+        List<DadosTemporada> temporadas = new ArrayList<>();
+		for (int i = 1; i <= dadosSerie.totalTemporadas() ; i++) {
+
+			json = consumoApi.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + API_KEY);
+			DadosTemporada dadosTemporada = converteDados.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+		temporadas.forEach(System.out::println);
     }
 }
