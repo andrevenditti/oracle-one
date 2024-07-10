7b10dd5a40dcfc2e37065f5b02006cf1ced85e79
Andre Venditti <am7venditti@gmail.com>
2024-07-10 00:27:47 -0300

    Feature: adding list for series

---
+++ springProjects/screenmatch/src/main/java/br/com/alura/screenmatch/model/Categoria.java
@@ -0,0 +1,23 @@
+package br.com.alura.screenmatch.model;
+
+public enum Categoria {
+    ACAO("Action"),
+    COMEDIA("Comedy"),
+    ROMANCE("Romance"),
+    CRIME("Crime"),
+    DRAMA("Drama");
+
+    private String categoriaOmdb;
+    Categoria(String categoriaOmdb) {
+        this.categoriaOmdb = categoriaOmdb;
+    }
+
+    public static Categoria fromString(String text) {
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.categoriaOmdb.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Nenhuma categoria encontrada para a string fornecida: " + text);
+    }
+}
--- springProjects/screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ springProjects/screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,11 +1,13 @@
 package br.com.alura.screenmatch.principal;
 
 import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.Serie;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 import br.com.alura.screenmatch.model.DadosTemporada;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 
 public class Principal {
@@ -16,34 +18,44 @@ public class Principal {
     private final String ENDERECO = "https://www.omdbapi.com/?t=";
     private final String API_KEY = "&apikey=ef6f5091";
 
+    private List<DadosSerie> dadosSeries = new ArrayList<>();
+
     public void exibeMenu() {
-        var menu = """
-                1 - Buscar séries
-                2 - Buscar episódios
-                
-                0 - Sair                                 
-                """;
+        var opcao = -1;
+        while (opcao != 0) {
+            var menu = """
+                    1 - Buscar séries
+                    2 - Buscar episódios
+                    3 - Listar series buscadas
+                                    
+                    0 - Sair                                 
+                    """;
 
-        System.out.println(menu);
-        var opcao = leitura.nextInt();
-        leitura.nextLine();
+            System.out.println(menu);
+            opcao = leitura.nextInt();
+            leitura.nextLine();
 
-        switch (opcao) {
-            case 1:
-                buscarSerieWeb();
-                break;
-            case 2:
-                buscarEpisodioPorSerie();
-                break;
-            case 0:
-                System.out.println("Saindo...");
-                break;
-            default:
-                System.out.println("Opção inválida");
+            switch (opcao) {
+                case 1:
+                    buscarSerieWeb();
+                    break;
+                case 2:
+                    buscarEpisodioPorSerie();
+                    break;
+                case 3:
+                    listarSeriesBuscadas();
+                    break;
+                case 0:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+            }
         }
     }
     private void buscarSerieWeb() {
         DadosSerie dados = getDadosSerie();
+        dadosSeries.add(dados);
         System.out.println(dados);
     }
 
@@ -66,4 +78,15 @@ public class Principal {
         }
         temporadas.forEach(System.out::println);
     }
+
+    private void listarSeriesBuscadas() {
+        List<Serie> series = new ArrayList<>();
+        series = dadosSeries.stream()
+                .map(d -> new Serie(d))
+                        .collect(Collectors.toList());
+        series.stream()
+                .sorted(Comparator.comparing(Serie::getGenero))
+                .forEach(System.out::println);
+    }
+
 }
