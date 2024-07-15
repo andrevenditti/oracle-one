43aebcaf5f9de3cc4a7d705814d6fd1d3249bc99
Andre Venditti <am7venditti@gmail.com>
2024-07-15 16:39:19 -0300

    Adding error handling to project

---
+++ springProjects/voll_med_api/src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -0,0 +1,33 @@
+package med.voll.api.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class TratadorDeErros {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarErro404() {
+        return ResponseEntity.notFound().build();
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
+        var erros = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream()
+                .map(ErroValidacaoDTO::new).collect(Collectors.toList()));
+    }
+
+    private record ErroValidacaoDTO(
+            String campo,
+            String mensagem
+    ) {
+        public ErroValidacaoDTO(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+    }
+}
