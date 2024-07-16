cc81b4001af87f03514c7d6f51f07f54679748f9
Andre Venditti <am7venditti@gmail.com>
2024-07-15 23:53:56 -0300

    Feature: token generation by login

--- springProjects/voll_med_api/src/main/java/med/voll/api/controller/AutenticacaoController.java
+++ springProjects/voll_med_api/src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -2,6 +2,9 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.dto.AutenticacaoDTO;
+import med.voll.api.infra.security.TokenDTO;
+import med.voll.api.model.usuario.Usuario;
+import med.voll.api.service.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,16 +14,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/login")
 public class AutenticacaoController {
         @Autowired
         private AuthenticationManager manager;
+        @Autowired
+        private TokenService tokenService;
         @PostMapping
         public ResponseEntity efetuarLogin(@RequestBody @Valid AutenticacaoDTO data) {
-            var token = new UsernamePasswordAuthenticationToken(data.login(), data.senha());
-            var autentication =  manager.authenticate(token);
+            var authenticationToken = new UsernamePasswordAuthenticationToken(data.login(), data.senha());
+            var authentication =  manager.authenticate(authenticationToken);
+            var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
 
-            return ResponseEntity.ok().build();
+            return ResponseEntity.ok(new TokenDTO(tokenJWT));
         }
 }
